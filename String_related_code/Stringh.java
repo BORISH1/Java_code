cc95e71f1c9ca82c9f4a7fe1a3f16fe09a50dfd3
Borish1 <borishningombam@gmail.com>
2025-03-12 22:25:57 +0530

    String

---
+++ String_related_code/Stringh.java
@@ -0,0 +1,12 @@
+public class Stringh {
+          public void concate(){
+                    String a = "Borish ";
+                    String b = "Ningombam";
+                    System.out.println(a +b);
+          }
+          public static void main(String args[]){
+                    Stringh obj = new Stringh();
+                    obj.concate();
+
+          }
+}
