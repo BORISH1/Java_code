1c8713fe79e9763ddfdde0b07faa94816d48f4d2
Borish1 <borishningombam@gmail.com>
2025-03-13 20:33:20 +0530

    this is simple program for swapping two integer using this method we can swap any datatype

---
+++ swap_two_integer/simple.java
@@ -0,0 +1,15 @@
+package swap_two_integer;
+
+public class simple {
+          public static void main(String args[]){
+                    int a = 4;
+                    int b = 2;
+                    int temp; // most programmer use temp for temporary to swapping any two integer or any two string or etc.
+
+                    temp = a;
+                    a = b;
+                    b = temp;
+                    System.out.println("a= "+a);
+                    System.out.println("b = " +b);
+          }
+}
