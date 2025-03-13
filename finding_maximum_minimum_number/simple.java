8a25ab1569675d80a982c4aa4c1a372f3263c88e
Borish1 <borishningombam@gmail.com>
2025-03-13 22:31:44 +0530

    simple program for finding the maximum and minimum

---
+++ finding_maximum_minimum_number/simple.java
@@ -0,0 +1,8 @@
+public class simple {
+          public static void main(String argsp[]){
+                double a = 344.3;
+                double b = 321.42;
+                double c = Math.max(a,b);
+                System.out.println("The maximum number between " + a + " and  " + b + " is " + c);
+          }
+}
