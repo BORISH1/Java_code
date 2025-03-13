0f4c48a3fdd12c30e19c3bbdd11fcc5800420b8a
Borish1 <borishningombam@gmail.com>
2025-03-13 23:17:07 +0530

    taking random number

---
+++ Random/simple.java
@@ -0,0 +1,11 @@
+package Random;
+
+import java.util.Random;
+
+public class simple {
+          public static void main(String args[]){
+                    Random obj = new Random();
+                    int x = obj.nextInt(10);
+                    System.out.println(x);
+          }
+}
