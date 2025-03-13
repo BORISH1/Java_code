d4fe58a85a81a4e27112b1f1a71350dabd4b6578
Borish1 <borishningombam@gmail.com>
2025-03-13 20:26:50 +0530

    this is the simple string swap program using java

---
+++ Swap_two_string/simple.java
@@ -0,0 +1,14 @@
+package Swap_two_string;
+
+public class simple {
+          public static void main(String args[]){
+                    String a = "Ningombam";
+                    String b = "Borish";
+                    String temp;
+                    temp = a;
+                    a = b;
+                    b = temp;
+                    System.out.println("a "+a);
+                    System.out.println("b "+b);
+          }
+}
