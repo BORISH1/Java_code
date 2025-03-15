84413096bb82bee7675cb9eb3dcbe7ef70621fc7
Borish1 <borishningombam@gmail.com>
2025-03-15 20:32:11 +0530

    this is simple program for the switch case

---
+++ Switch_case/simple.java
@@ -0,0 +1,24 @@
+package Switch_case;
+
+public class simple {
+          public static void main(String args[]){
+                    String day = "pizza";
+                    switch(day){
+                              case "sunday" : System.out.println("It is sunday");
+                              break;
+                              case "monday" : System.out.println("It is monday");
+                              break;
+                              case "tuesday" : System.out.println("It is tuesday");
+                              break;
+                              case "wednusday" : System.out.println("It is wednusday");
+                              break;
+                              case "thursday" : System.out.println("It is thursday");
+                              break;
+                              case "friday" : System.out.println("It is friday");
+                              break;
+                              case "saturday" : System.out.println("It is saturday");
+                              break;
+                              default: System.out.println("you are entered the wrong input");
+                    }
+          }
+}
