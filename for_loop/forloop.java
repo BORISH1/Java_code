04ad2a04e4ecbdf8006f481fa23ca5b716b66fc2
Borish1 <borishningombam@gmail.com>
2025-05-28 12:13:28 +0530

    loop simple code

---
+++ for_loop/forloop.java
@@ -0,0 +1,12 @@
+public class forloop{
+          public static void main(String args[]){
+                    for(int i =1;i<=4;i++)
+                    {
+                              System.out.println("day"+i);
+                              for(int j=1;j<=8;j++)
+                              {
+                                        System.out.println(" "+(j+8)+ "-"+(j+9));
+                              }
+                    }
+          }
+}
